15437e6ee8ca581bf3bea21165a9d4d36cbb593a
paulodinizlima <diniz_lima@hotmail.com>
2026-01-11 13:45:59 -0300

    Início

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Text } from 'react-native';
+
+
+export default function App(){
+    return(
+        <View style={styles.container}>
+            <Text>Sujeito Programador</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
